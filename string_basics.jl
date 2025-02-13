55d93cc18c3707a700cc80d32854f643886a99a6
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2025-02-13 15:09:16 +0100

    further examples for string usage

--- string_basics.jl
+++ string_basics.jl
@@ -13,3 +13,18 @@ s1 = "Hello there!"
 s1_caps = uppercase(s1)
 s1_lower = lowercase(s1) # if print isnt used, the latest line is printed
 println(s1_caps, "\n", s1_lower)
+
+show(s1[5]); println() # indexing starts at 1 instead of 0 like in Python
+show(s1[1:5]); println() # slicing is similar to Python, last character is included
+
+a = "Hello"
+b = "me"
+println("$a to $b") # string interpolation
+
+println("1 + 2 = $(1 + 2)")
+
+s2 = "this" * " and" * " that" # string concatenation with *
+println(s2)
+
+s3 = string("this", " and", " that") # string concatenation with string()
+println(s3)
