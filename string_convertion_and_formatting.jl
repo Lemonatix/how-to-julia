6a47b7db9c5a75e28c5a85a5d9d4da673fd29b7c
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2025-02-16 17:50:56 +0100

    formatting example

--- string_convertion_and_formatting.jl
+++ string_convertion_and_formatting.jl
@@ -1,4 +1,12 @@
+# strings convertion using float and int
 e_str1 = "2.718"
 e = parse(Float64, e_str1)
-println(e)
-print(5e)
+println(5e)
+
+num_15 = parse(Int, "15")
+println(3num_15)
+
+# string convertion and formatting
+using Printf # import Printf module
+@printf "e = %0.2f\n" e # precision of 2 decimal places, \n is newline
+e_str2 = @sprintf("%0.3f", e)
