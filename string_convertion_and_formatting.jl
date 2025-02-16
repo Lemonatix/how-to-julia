47fe0cf72f0e701ae4f8ea77cdcb661d635e1f12
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2025-02-16 18:04:32 +0100

    further examples of string convertion and formatting

--- string_convertion_and_formatting.jl
+++ string_convertion_and_formatting.jl
@@ -10,3 +10,18 @@ println(3num_15)
 using Printf # import Printf module
 @printf "e = %0.2f\n" e # precision of 2 decimal places, \n is newline
 e_str2 = @sprintf("%0.3f", e)
+
+# show that 2 strings are the same
+println("e_str1 == e_str2: $(e_str1 == e_str2)") # e_str1 == e_str2 is enough
+
+# available number format characters are f, e, a, g, c, s, p, d:
+@printf "fix trailing precision: %0.3f\n" float(pi)
+@printf "scientific form: %0.6e\n" 1000pi
+@printf "float in hexadecimal format: %a\n" 0xff
+@printf "fix trailing precision: %g\n" pi*1e8
+@printf "a character: %c\n" 'α'
+@printf "a string: %s\n" "look I'm a string!"
+@printf "right justify a string: %50s\n" "width 50, text right justified!"
+#> right justify a string:                    width 50, text right justified!
+@printf "a pointer: %p\n" 100000000
+@printf "print an integer: %d\n" 1e10
