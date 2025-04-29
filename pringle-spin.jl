83343c746f79aa5a905ccabbec4413c53377b354
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2025-04-29 16:07:25 +0200

    changes speed of rotation speed

--- pringle-spin.jl
+++ pringle-spin.jl
@@ -8,10 +8,10 @@ f(x, y) = x^2 / a^2 - y^2 / b^2
 
 x = -7:0.1:7
 y = -7:0.1:7
-a = 1
-b = 1
+a = 1.0
+b = 1.0
 
-anim = @animate for azimuth in 0:8:360
+anim = @animate for azimuth in 0:10:360
     plot(
         x, y, f,
         st=:surface,
