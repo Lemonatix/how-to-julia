79b9e3fbb63f7c8962579cab8e93cf60aa69f9a2
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2025-04-29 12:48:48 +0200

    fish spin pringle

---
+++ pringle-spin.jl
@@ -0,0 +1,20 @@
+import Pkg
+using Plots
+
+# Use the GR backend for animation support
+gr()
+
+f(x, y) = x^2 / a^2 - y^2 / b^2
+
+x = -7:0.1:7
+y = -7:0.1:7
+a = 1
+b = 1
+
+# Create an animated plot
+anim = @animate for azimuth in 0:9:360
+    plot(x, y, f, st=:surface, title="Hyperbolic Paraboloid", xlabel="x", ylabel="y", zlabel="f(x,y)", legend=false, camera=(azimuth, 30))
+end
+
+# Save the animation as a GIF
+gif(anim, "hyperbolic_paraboloid_rotation.gif", fps=30)
