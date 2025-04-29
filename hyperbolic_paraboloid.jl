d7d6e885969e11a4bd7342c736805d3e13e72d68
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2025-04-29 12:40:48 +0200

    changes dimensions again

--- hyperbolic_paraboloid.jl
+++ hyperbolic_paraboloid.jl
@@ -5,8 +5,8 @@ plotly()
 
 f(x,y) = x^2/a^2 - y^2/b^2
 
-x = -5:0.1:5
-y = -5:0.1:5
+x = -7:0.1:7
+y = -7:0.1:7
 a = 1
 b = 1
 plot(x,y, f, st=:surface, title="Hyperbolic Paraboloid", xlabel="x", ylabel="y", zlabel="f(x,y)", legend=false)
