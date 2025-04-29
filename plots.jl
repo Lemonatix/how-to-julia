55e97b80437030c179289db32a1941dab26f2691
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2025-04-29 11:14:02 +0200

    rename function

--- plots.jl
+++ plots.jl
@@ -6,4 +6,4 @@ f(x,y) = x^2-y^2
 x = -10:0.1:10
 y = -10:0.1:10
 
-plot(x,y, f, st=:surface, title="3D Surface Plot", xlabel="x", ylabel="y", zlabel="f(x,y)", legend=false)
+plot(x,y, f, st=:surface, title="Hyperbolic Paraboloid", xlabel="x", ylabel="y", zlabel="f(x,y)", legend=false)
