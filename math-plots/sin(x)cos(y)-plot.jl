d9b78cb772d77e46e59d747acd2e8c8a18f3c526
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2025-05-06 12:35:25 +0200

    some small adjustments

--- math-plots/sin(x)cos(y)-plot.jl
+++ math-plots/sin(x)cos(y)-plot.jl
@@ -7,4 +7,4 @@ f(x,y) = sin(x) * cos(y)
 
 x = range(-5, 5, length=700)
 y = range(-5, 5, length=700)
-plot(x,y, f, st=:surface, title="x²-y³", xlabel="x", ylabel="y", zlabel="f(x,y)", legend=false)
+plot(x,y, f, st=:surface, title="sin(x)cos(y)", xlabel="x", ylabel="y", zlabel="f(x,y)", legend=false)
