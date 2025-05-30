fe044aad16f11dc90988df9aba0ce3aa0ee0c73a
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2025-05-30 20:00:53 +0200

    rename and create file

---
+++ math-calc/grad_descent_numeric.jl
@@ -0,0 +1,16 @@
+function grad_f(x)
+    x1, x2 = x
+    return [cos(x1)*cos(x2), -sin(x1)*sin(x2)]
+end
+
+function run_gradient_descent()
+    tau = 1.0
+    x   = [1.0, 1.0]
+
+    for k in 1:4
+        x = x .- tau .* grad_f(x)
+        println("k = $k: x = ", round.(x, digits=6))
+    end
+end
+
+run_gradient_descent()
