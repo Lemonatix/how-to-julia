be25127c24d01d2de940d8defea7cfb4bdde1d7c
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2025-05-30 20:15:29 +0200

    push last changes

---
+++ math-calc/grad_descent_numeric.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import ace_tools as tools
+
+def grad_f(x):
+    x1, x2 = x
+    df_dx1 = np.cos(x1) * np.cos(x2)
+    df_dx2 = -np.sin(x1) * np.sin(x2)
+    return np.array([df_dx1, df_dx2])
+
+tau = 1.0
+x = np.array([1.0, 1.0])
+iterates = [x.copy()]
+
+for k in range(4):
+    x = x - tau * grad_f(x)
+    iterates.append(x.copy())
+
+df = pd.DataFrame(iterates[1:], columns=['x1', 'x2'], index=[f'k={i}' for i in range(1,5)])
+tools.display_dataframe_to_user(name="Gradientenverfahren-Schritte (Python)", dataframe=df)
