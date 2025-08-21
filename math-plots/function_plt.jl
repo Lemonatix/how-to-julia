58e655ec585d90dd50dcca7a19abff3e367325c3
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2025-08-21 23:37:36 +0200

    adjusts title

--- math-plots/function_plt.jl
+++ math-plots/function_plt.jl
@@ -7,4 +7,4 @@ f(x,y) = x*y^2 / (x^2 + y^2)
 
 x = -7:0.1:7
 y = -7:0.1:7
-plot(x,y, f, st=:surface, title="x*y / (x^2 + y^2)", xlabel="x", ylabel="y", zlabel="f(x,y)", legend=false)
+plot(x,y, f, st=:surface, title="x*y^2 / (x^2 + y^2)", xlabel="x", ylabel="y", zlabel="f(x,y)", legend=false)
